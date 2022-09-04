bc7d800378ca096e8d3f08d02e57dceebbdfabc9
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2022-09-04 14:05:41 +0800

    init DDD Project

---
+++ src/Domain/Shared/Models/BaseModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Domain\Shared\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseModel extends Model
+{
+    use HasFactory;
+
+    protected static function newFactory()
+    {
+        $parts = str(get_called_class())->explode('\\');
+        $domain = $parts[1];
+        $model = $domain->last();
+
+        return app("Database\\Factories\\{$domain}\\{$model}Factory");
+    }
+}
